7ff033659bbd41ae95b49e6f7e08e2355dc85413
Lera <valeria.usatenko@gmail.com>
2026-01-11 00:49:18 +0100

    Fix DB port to use Number()

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,14 @@ import * as mysql from 'mysql2/promise'
 
 const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: process.env.DB_PORT||51501 ,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
-
+    database: process.env.DB_NAME,
+    ssl: {
+    rejectUnauthorized: false // Souvent nécessaire pour les certificats auto-signés en mode gratuit
+  }
+});
 
 
 connection
